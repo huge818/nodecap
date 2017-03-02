1cb0c4664ffcbcd244469fb95873d65532d3e280
huzhongchuan <huzhongchuan@os-easy.com>
2017-03-02 15:56:08 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# nodecap
-A cross-platform binding for performing packet capturing with [node.js]
+
+Description
+===========
+
+A cross-platform binding for performing packet capturing with [node.js](http://nodejs.org/).
+
+Requirements
+============
+
+* [node.js](http://nodejs.org/) -- v0.12.18 or older (you can build it on latest nodejs)
+
+* For Windows: [WinPcap](http://www.winpcap.org/install/default.htm)
+
+* For *nix: libpcap and libpcap-dev/libpcap-devel packages
+
+
+Install
+============
+
+    npm install nodecap
+
+
+Examples
+========
+
+* Send an arbitrary packet: An arp request for example
+
+```javascript
+var nodecap=require('nodecap');
+var Cap =  new nodecap();
+var buffer = new Buffer ([
+    // ETHERNET
+    //...
+]);
+
+try {
+  // send will not work if pcap_sendpacket is not supported by underlying `device`
+  c.sendBufer(buffer);
+} catch (e) {
+  console.log("Error sending packet:", e);
+}
+
+```
+
+* List all network devices:
+
+API
+===
+
+Cap events
+----------
+
+* **capture**(< _integer_ >nbytes, < _boolean_ >truncated) - A packet `nbytes` in size was captured. `truncated` indicates if the entire packet did not fit inside the _Buffer_ supplied to open().
+
+
+nodecap methods
+-----------
+sendBufer   
+close
+
---
+++ nodecap.js
@@ -0,0 +1,115 @@
+
+
+var Cap = require('cap').Cap;
+var decoders = require('cap').decoders;
+var PROTOCOL = decoders.PROTOCOL;
+
+ Buffer.prototype.subArr=function(n,L){
+ 	return this.slice(n,n+L);
+ }
+
+var nodecap=function(){
+	var self=this;
+
+	this.protocol={
+		"udp":PROTOCOL.IP.UDP,
+		"tcp":PROTOCOL.IP.TCP,
+		"SCTP":PROTOCOL.IP.SCTP
+	};
+
+	/**
+	 * [capture 开始捕获]
+	 * @param  {[type]}   obj [捕获参数]
+	 * @param  {Function} fn  [回调]
+	 */
+	 this.capture=function(obj,fn){
+		var Caper = new Cap();
+		var device = Cap.findDevice(obj.device);
+		if(!device){
+			console.log("device不正确");
+			return;
+		}
+		var filter = obj.filter;
+		var bufSize = 10 * 1024 * 1024;
+		var buffer = new Buffer(65535);
+		var linkType = Caper.open(device, filter, bufSize, buffer);
+		Caper.setMinBytes && Caper.setMinBytes(0);
+		//console.log("Caper",Caper);
+		var N=0;
+		Caper.on('packet', function(nbytes, trunc) {
+			if(linkType !== 'ETHERNET'){
+				console.log("非以太网数据包");
+				return;
+			}
+
+			var ret = decoders.Ethernet(buffer);
+			if(ret.info.type !== PROTOCOL.ETHERNET.IPV4){
+				console.log("IPV6");
+				return;
+			}
+			ret = decoders.IPV4(buffer, ret.offset);
+			var resdata={
+				linkType:linkType,
+				ipType:ret.info.type,
+				rawData:buffer.slice(0, nbytes),
+				srcaddr:ret.info.srcaddr,
+				dstaddr:ret.info.dstaddr,
+				srcport:ret.info.srcport,
+				dstport:ret.info.dstport,
+				ret:ret,
+				RAWPROTOCOL:PROTOCOL
+			};
+
+			if (ret.info.protocol === PROTOCOL.IP.TCP) {
+				var datalen = ret.info.totallen - ret.hdrlen;
+				ret = decoders.TCP(buffer, ret.offset);
+				datalen -= ret.hdrlen;
+				resdata.dataLength=datalen;
+				resdata.protocol="tcp";
+				var rawData=buffer.slice(0, nbytes).toString("hex");
+				if(N==0){
+					console.log(0,buffer.subArr(0, 6).toString("hex")); // 目的mac地址 
+					console.log(1,buffer.subArr(6, 6).toString("hex"));  // 源mac地址 
+					console.log(2,buffer.subArr(12,2));  // 类型 
+					console.log(3,buffer.subArr(14,12));  // 版本长度，标识等。。。
+					console.log(4,buffer.subArr(26,4).toString("hex"));  // 32位源IP 
+					console.log(5,buffer.subArr(30,4).toString("hex"));  // 32位目的IP 
+				}
+				fn&&fn(resdata);
+				return;
+			}
+
+			if(ret.info.protocol === PROTOCOL.IP.UDP) {
+				ret = decoders.UDP(buffer, ret.offset);
+				resdata.protocol="udp";
+				fn&&fn(resdata);
+				return;
+			} 
+			var protocol=PROTOCOL.IP[ret.info.protocol];
+			console.log('暂不支持的IPv4协议: ' + protocol);
+			N++;
+		});
+		self.Caper=Caper;
+	 }
+
+	 this.sendBufer=function(bufer,fn){
+		try {
+		  self.Caper.send(buffer, buffer.length);
+		  fn&&fn();
+		} catch (e) {
+		  console.log("Error", e);
+		}
+	 }
+
+	 /**
+	  * [close 停止捕获]
+	  */
+	 this.close=function(fn){
+		if(self.Caper){
+			 self.Caper.close();
+			 fn&&fn();
+		}
+	 }
+}
+
+module.exports = nodecap;
